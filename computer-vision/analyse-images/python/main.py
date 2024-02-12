79a31275d0d606735f0f9dab29aa0d346fa486a8
Roy Luo <r.luo2418@gmail.com>
2024-02-12 17:51:44 +1100

    Add computer vision

---
+++ computer-vision/analyse-images/python/main.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+import os
+
+def main():
+  load_dotenv()
+  client = ImageAnalysisClient(endpoint=os.getenv('API_ENDPOINT'), credential=AzureKeyCredential(os.getenv('API_KEY')))
+
+  analyse_image(client)
+
+def analyse_image(client):
+  res = client.analyze(
+    image_url="https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png", 
+    visual_features=[VisualFeatures.TAGS]
+  )
+  print(res)
+
+if __name__ == "__main__":
+  main()
